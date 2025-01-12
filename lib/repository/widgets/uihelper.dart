7f0dd6d52dcc0f38d61abc0703075181f8278783
gokulIosDev <gokuliosdev1@gmail.com>
2025-01-12 17:30:19 +0530

    Update uihelper.dart

--- lib/repository/widgets/uihelper.dart
+++ lib/repository/widgets/uihelper.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class UiHelper {
   static CustomInage({required String url}) {
@@ -8,10 +7,10 @@ class UiHelper {
 
   static CustomTExt(
       {required String text,
-      Color? color,
-      String? fontFamily,
-      FontWeight? fontWeight,
-      double? fontSize}) {
+        Color? color,
+        String? fontFamily,
+        FontWeight? fontWeight,
+        double? fontSize}) {
     return Text(
       text,
       style: TextStyle(
@@ -24,24 +23,24 @@ class UiHelper {
 
   static CustomButton(
       {required VoidCallback callBack,
-      Color? foreGround,
-      Color? backGround,
-      double? height,
-      double? width,
-      required String text}) {
+        Color? foreGround,
+        Color? backGround,
+        double? height,
+        double? width,
+        required String text}) {
     return SizedBox(
-      height: height ?? 60.h,
-      width: width ?? 360.w,
+      height: height ?? 60,
+      width: width ?? 360,
       child: ElevatedButton(
           onPressed: callBack,
           style: ElevatedButton.styleFrom(
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.w)),
+                  borderRadius: BorderRadius.circular(10)),
               backgroundColor: backGround ?? Color(0XFF1F41BB)),
           child: Text(
             text,
             style: TextStyle(
-                fontSize: 18.sp,
+                fontSize: 18,
                 fontWeight: FontWeight.normal,
                 fontFamily: "Regular",
                 color: foreGround ?? Color(0XFFFFFFFF)),
@@ -49,40 +48,41 @@ class UiHelper {
     );
   }
 
-  static CustomTextField(
-      {required TextInputType keyBoardStyle,
-      required TextEditingController controller,
-      required String text,
-      required bool isHide}) {
+  static CustomTextField({
+    required TextInputType keyBoardStyle,
+    required TextEditingController controller,
+    required String text,
+    required bool isHide,
+  }) {
     return Padding(
       padding: const EdgeInsets.only(left: 20, right: 20),
       child: Container(
-        height: 45.h,
-        width: 350.w,
+        height: 45,
+        width: 350,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.w),
+          borderRadius: BorderRadius.circular(10),
           color: Color(0XFFF1F4FF),
-          border: Border.all(color: Color(0XFF1F41BB)),
+          border: Border.all(color: Colors.grey),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: TextField(
-            controller: controller,
-            obscureText: isHide,
-            keyboardType: keyBoardStyle,
-            decoration: InputDecoration(
-                hintText: text,
-                hintStyle: TextStyle(
-                  color: Color(0XFF626262),
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.w500,
-                ),
-                border: InputBorder.none),
+        child: TextField(
+          controller: controller,
+          obscureText: isHide,
+          keyboardType: keyBoardStyle,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10), // Adjust padding here
+            hintText: text,
+            hintStyle: TextStyle(
+              color: Color(0XFF626262),
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+            border: InputBorder.none,
           ),
         ),
       ),
     );
   }
+
 
   static CustomSnackBar({required String text, required BuildContext context}) {
     return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -96,14 +96,14 @@ class UiHelper {
         required String text,
         required VoidCallback callback}) {
     return SizedBox(
-      height: 35.h,
-      width: 280.w,
+      height: 35,
+      width: 280,
       child: TextField(
         textAlign: TextAlign.justify,
         controller: controller,
         decoration: InputDecoration(
             hintText: text,
-            hintStyle:  TextStyle(color: Color(0XFF818181),fontSize: 12.sp),
+            hintStyle:  TextStyle(color: Color(0XFF818181),fontSize: 12),
             suffixIcon: IconButton(
                 onPressed: () {
                   callback();
